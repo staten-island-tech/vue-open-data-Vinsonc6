44acebeb6ef0397e539c348213c8da8fece644c6
Vinsonc6 <vinsonc6@nycstudents.net>
2026-03-17 12:32:08 -0400

    3

--- VOD/src/App.vue
+++ VOD/src/App.vue
@@ -1,4 +1,43 @@
-<script setup></script>
+<script setup>
+import { ref, onMounted } from 'vue';
+
+const posts = ref([]);
+const isLoading = ref(true);
+const error = ref(null);
+
+async function fetchPosts() {
+  try {
+    const response = await fetch('https://data.cityofnewyork.us/resource/hc8x-tcnd.json');
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    const data = await response.json();
+    posts.value = data;
+  } catch (err) {
+    error.value = err.message;
+  } finally {
+    isLoading.value = false;
+  }
+}
+
+const result = filter
+
+
+const filterData()
+
+
+
+</script>
+
+<template>
+  <div v-if="isLoading">Loading posts...</div>
+  <div v-else-if="error">Error: {{ error }}</div>
+  <ul v-else>
+    <li v-for="post in posts" :key="post.id">
+      {{ post.title }}
+    </li>
+  </ul>
+</template>
 
 <template>
   <h1>You did it!</h1>
@@ -7,5 +46,8 @@
     documentation
   </p>
 </template>
+
+
+
 
 <style scoped></style>
