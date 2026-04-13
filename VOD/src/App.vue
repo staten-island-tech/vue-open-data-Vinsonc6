6a6022b28fb3cebf70d0d0c4df3da09085620ec9
Vinsonc6 <vinsonc6@nycstudents.net>
2026-04-12 22:52:35 -0400

    e
    e

--- VOD/src/App.vue
+++ VOD/src/App.vue
@@ -15,22 +15,9 @@ import { ref, onMounted } from 'vue'
 const firehouses = ref([])
 ForeEach()
 onMounted(async () => {
-  const res = await fetch('https://data.cityofnewyork.us/resource/hc8x-tcnd.json')
+  const res = await fetch('https://data.cityofnewyork.us/resource/hc8x-tcnd.json?limit=10')
   firehouses.value = await res.json()
 })
 
-firehouses.ForEach(Brooklyn => {
-if (borough === 0) {
-  console.log (firehouses.name)
-}
-firehouses.ForEach(Mahattan => {
-  if (borough === 0)
-}
-}
-)
-}
-
-
-)
 </script>
 
